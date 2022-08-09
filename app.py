d24877cee4c7a512ba50bdd107c696ccd68e7dbf
s0urav21 <100081972+s0urav21@users.noreply.github.com>
2022-08-09 18:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,46 @@ user_input=st.text_input('Age')
 pred_data.loc[0,'Age']=user_input
 
 
+user_input=st.text_input('Hospital_type_code')
+pred_data.loc[0,'Hospital_type_code']=user_input
+
+user_input=st.number_input('City_Code_Hospital')
+pred_data.loc[0,'City_Code_Hospital']=user_input
+
+user_input=st.text_input('Hospital_region_code')
+pred_data.loc[0,'Hospital_region_code']=user_input
+
+user_input=st.number_input('Available Extra Rooms in Hospital')
+pred_data.loc[0,'Available Extra Rooms in Hospital']=user_input
+
+
+user_input=st.text_input('Department')
+pred_data.loc[0,'Department ']=user_input
+
+user_input=st.text_input('Ward_Type')
+pred_data.loc[0,'Ward_Type']=user_input
+
+user_input=st.text_input('Ward_Facility_Code')
+pred_data.loc[0,'Ward_Facility_Code']=user_input
+
+
+user_input=st.number_input('Bed Grade')
+pred_data.loc[0,'Bed Grade']=user_input
+
+user_input=st.number_input('City_Code_Patient')
+pred_data.loc[0,'City_Code_Patient']=user_input
+
+user_input=st.text_input(' Type of Admission')
+pred_data.loc[0,' Type of Admission']=user_input
+
+user_input=st.number_input(' Severity of Illness')
+pred_data.loc[0,' Severity of Illness']=user_input
+
+user_input=st.number_input(' Visitors with Patient')
+pred_data.loc[0,' Visitors with Patient']=user_input
+
+user_input=st.number_input('  Admission_Deposit')
+pred_data.loc[0,'  Admission_Deposit']=user_input
 
 
 model=pickle.load(open('catboost1.pkl', 'rb'))
